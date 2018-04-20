dd0858b8f7ee4083f701ba012713ae8d19f5d8b9
BenAvans <bkrocke@avans.nl>
2018-04-20 21:43:28 +0200

    Creating an ellipse

---
+++ WPF_Practice/ShapeAnEllipse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+
+namespace WPF_Practice
+{
+    class ShapeAnEllipse : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new ShapeAnEllipse());
+        }
+
+        public ShapeAnEllipse()
+        {
+            Title = "Shape an Ellipse";
+
+            Ellipse ellipse = new Ellipse();
+            ellipse.Fill = Brushes.AliceBlue;
+            ellipse.StrokeThickness = 24;
+            ellipse.Stroke = new LinearGradientBrush(Colors.CadetBlue, Colors.Chocolate, new Point(1, 0), new Point(0, 1));
+            ellipse.Width = 300;
+            ellipse.Height = 300;
+            Content = ellipse;
+        }
+
+    }
+}

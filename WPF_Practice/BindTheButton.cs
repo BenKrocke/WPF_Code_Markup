a4fc22278be08c13d5a454a682e94cc56a3fdd84
BenAvans <bkrocke@avans.nl>
2018-04-21 11:13:17 +0200

    Commands & Bindings

---
+++ WPF_Practice/BindTheButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace WPF_Practice
+{
+
+    public class BindTheButton : Window
+    {
+        private bool isToggled = false;
+
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new BindTheButton());
+        }
+        public BindTheButton()
+        {
+            Title = "Bind the button";
+
+            ToggleButton btn = new ToggleButton();
+            btn.ToolTip = "This is my tooltip";
+            btn.Content = "Make _Topmost";
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.VerticalAlignment = VerticalAlignment.Center;
+            btn.SetBinding(ToggleButton.IsCheckedProperty, "Topmost");
+            btn.DataContext = this;
+            Content = btn;
+        }
+    }
+        
+}
---
+++ WPF_Practice/CommandTheButton.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPF_Practice
+{
+    class CommandTheButton : Window
+    {
+
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new CommandTheButton());
+        }
+        public CommandTheButton()
+        {
+            Title = "Bind the button";
+
+            Button btn = new Button();
+            btn.ToolTip = "This is my tooltip";
+            btn.Content = "Show alert";
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.VerticalAlignment = VerticalAlignment.Center;
+            btn.Command = ApplicationCommands.Paste;
+            btn.Content = ApplicationCommands.Paste.Text;
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Paste, OnPasteExecuted, CanPasteExecute));
+            Content = btn;
+        }
+
+        private void OnPasteExecuted(object sender, ExecutedRoutedEventArgs args)
+        {
+            Title = Clipboard.GetText();
+        }
+        
+        private void CanPasteExecute(object sender, CanExecuteRoutedEventArgs args)
+        {
+            args.CanExecute = Clipboard.ContainsText();
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+            Title = "Command the button";
+        }
+    }
+}
